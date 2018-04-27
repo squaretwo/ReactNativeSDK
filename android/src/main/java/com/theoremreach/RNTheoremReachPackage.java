f7b24e036b53bb6ddc32d5abc1fe88be1a803dec
Raymond Cook <lunarraid@gmail.com>
2018-04-27 15:13:19 -0400

    Remove deprecated override

--- android/src/main/java/com/theoremreach/RNTheoremReachPackage.java
+++ android/src/main/java/com/theoremreach/RNTheoremReachPackage.java
@@ -16,11 +16,6 @@ public class RNTheoremReachPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
     }
